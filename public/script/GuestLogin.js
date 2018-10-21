e9b48f4e10a2d1c1740b3dee5c0b678cba51b0e7
Guruvigneshwaran Sivasubramanian <gursivas@publicisgroupe.net>
2018-10-22 02:48:42 +0530

    committing to master

---
+++ public/script/GuestLogin.js
@@ -0,0 +1,93 @@
+$( document ).ready(function() {
+    $('#enteredUserDetails').hide();
+    $('#userDetailsEnter').show();
+});
+
+function toggleDisplay(){
+    if($('#userDetailsEnter').attr('style')=='display: none;'){
+        $('#userDetailsEnter').show();
+        $('#enteredUserDetails').hide();
+    }else if($('#enteredUserDetails').attr('style')=='display: none;'){
+        $('#userDetailsEnter').show();
+        $('#enteredUserDetails').hide();
+    }
+}
+
+function GoButtonClick() {
+    var userName = $('#userName').val();
+    $('#enteredUserDetails').show();
+    $('#userDetailsEnter').hide();
+    if(userName){
+        $('#enteredUserName').html('');
+        $('#enteredUserName').html($('#userName').val());
+    }else{
+        socket.emit('getGuestNumber','guest');
+    }    
+}
+
+var socket = io();
+
+function runScript(e){
+    if (e.keyCode == 13) {
+        send();
+    }
+}
+
+var rxMsgCount = 0;
+
+function send(){
+    if($('#chatMsg').val()){
+        var guestUserName = '';
+        var msgObject = '';
+        var msgToSend = $('#chatMsg').val();
+        if($('#userName').val()){
+            guestUserName = $('#userName').val();
+            msgObject = {
+                userName: guestUserName,
+                msg:msgToSend 
+            };
+            socket.emit('chat message', msgObject);
+            $('#chatMsg').val('');
+        }else{
+            socket.emit('getGuestNumber','guest');
+        }
+    }
+}
+socket.on("chat message1", function(dataFromServer){
+    ++rxMsgCount;
+    var msg = dataFromServer ;
+    var sentMsg = '';
+    if(dataFromServer.userName == $('#userName').val()){
+        sentMsg = '<div id=rxMsgCount'+rxMsgCount+' class = "align-right"><div class="name-left"><strong>';
+    }else{
+        sentMsg = '<div id=rxMsgCount'+rxMsgCount+' class = "align-left"><div class="name-right"><strong>';
+    }
+    sentMsg = sentMsg + dataFromServer.userName + '</strong></div>';
+    sentMsg = sentMsg + '<div class="chatText">'+dataFromServer.msg;
+    sentMsg = sentMsg + '</div></div>';
+    $($.parseHTML(sentMsg)).appendTo($('#chatArea'));
+    
+    var offsetTopValue = $('#rxMsgCount'+rxMsgCount).offsetTop;
+    $('#chatArea').scrollTop = offsetTopValue;
+});
+
+socket.on('guestNumberFromServer',function(dataFromServer){
+    var msgToSend = $('#chatMsg').val();
+    if(!$('#userName').val()){
+        $('#userName').val(dataFromServer);
+        GoButtonClick();
+        if(msgToSend){
+            var guestUserName = dataFromServer;
+            msgObject = {
+                userName: guestUserName,
+                msg: msgToSend
+            };
+            socket.emit('chat message', msgObject);
+            $('#chatMsg').val('');
+        }
+    }
+});
+
+socket.on('userCount',function(dataFromServer){
+    $('#numberOfActiverUsers').html(dataFromServer);
+});
--- server.js
+++ server.js
@@ -3,31 +3,59 @@ var app = express();
 var fs = require('fs');
 var https = require('http');
 
-var httpsPort = 3001;
+var httpsPort = 80;
 
 const options = {
     key: fs.readFileSync('./key.pem', 'utf8'),  
     cert: fs.readFileSync('./server.crt', 'utf8') 
 };
 
-var secureServer = https.createServer(app).listen(httpsPort, () => {  
+var secureServer = https.createServer(app).listen(httpsPort, () => {
     console.log(">> CentraliZr listening at port " + httpsPort);  
 });  
 
 var io = require('socket.io')(secureServer);
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/public/index.html');
+    res.sendFile(__dirname+'/public/html/GuestLogin.html');
 });
+
+app.get('/index.html',(req,res)=>{
+    var uName = req.param;
+});
+
+i = 0;
 
 io.on('connection', (socket)=>{
     console.log('a user connected');
+    var msg = null;
+    io.emit('userCount', 'Number Of Users Online:  '+ ++i);
+
     socket.on('chat message', (msg)=>{
-        console.log('received msg from client: '+msg);
-        var msg = msg.replace(/<[^>]+>/g, '');
+        console.log('received msg from '+msg.userName+':'+msg.msg);
         io.emit('chat message1',msg);
+    });
+
+    socket.on('getGuestNumber', (msg)=>{
+        io.emit('guestNumberFromServer',msg+'/_/'+'guest0'+i);
+    });
+
+    socket.on('disconnect', ()=>{
+        io.emit('userCount', 'Number Of Users Online:  '+ --i);
     });
 });
 
+app.get('/script/GuestLogin.js',(req,res)=>{
+    res.sendFile(__dirname+'/public/script/GuestLogin.js');
+});
+
+app.get('/images/Untitled.png',(req,res)=>{
+    res.sendFile(__dirname+'/public/images/Untitled.png');
+});
+
+app.get('/images/background.jpg',(req,res)=>{
+    res.sendFile(__dirname+'/public/images/background.jpg');
+});
+
 app.get('/css/styles.css',(req,res)=>{
-    res.sendFile(__dirname+'/css/styles.css');
+    res.sendFile(__dirname+'/public/css/styles.css');
 });
